5565eb0715fb59079cde78ef40ae0f85df5de276
Eric Stern <eric@ericstern.com>
2017-12-31 15:30:24 -0800

    progress?

--- index.js
+++ index.js
@@ -44,20 +44,39 @@ class Platform {
     if (ports != 8 && ports != 4) {
       throw new Error('Bad number of ports');
     }
-    let _switches = [];
+
+    const sw = new HDMISwitch(this.log, host);
+
+    let _ports = [];
     for (let i = 1; i <= ports; i++) {
       this.log("Creating switch " + i);
 
-      const acc = new Switch(this.log, i); // (this.api.hap, this.log, sw);
-      _switches.push(acc);
+      const acc = new Port(sw, this.log, i); // (this.api.hap, this.log, sw);
+      _ports.push(acc);
     }
 
-    callback(_switches);
+    callback(_ports);
   }
 }
 
-class Switch {
-  constructor(log, num) {
+class HDMISwitch {
+  constructor(log, host) {
+    this.log = log;
+    this.host = host;
+  }
+
+  getCurrentPort = async () => {
+    this.log("Fetching current port");
+    const result = await fetch(this.host);
+    const data = await result.blob();
+    const re = /Input: port([1-8])/;
+    return re.match(data);
+  }
+}
+
+class Port {
+  constructor(sw, log, num) {
+    this.switch = sw;
     this.log = log;
     this.num = num;
     this.name = "HDMI " + num;
@@ -85,7 +104,9 @@ class Switch {
   }
 
   getState(cb) {
-    this.log.debug('getstate' + this.num);
+    this.log('getstate' + this.num);
+    const cp = this.switch.getCurrentPort();
+    console.log(cp);
     cb(null, true); // todo: http GET state
   }
 
