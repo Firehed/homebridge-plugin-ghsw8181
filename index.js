31092c6e5682d03f7657c4a2c60eaf37b8acd127
Eric Stern <eric@ericstern.com>
2017-12-31 16:17:15 -0800

    try different polyfill

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var Accessory, Characteristic, Service, UUIDGen;
 
 const platformName = 'homebridge-plugin-ghsw8181';
 const platformPrettyName = 'GHSW8181';
-import 'whatwg-fetch';
+const fetch = require('node-fetch');
 
 module.exports = (homebridge) => {
   Accessory = homebridge.platformAccessory;
